39c671e4c69d0acdc35bb5e697345694e3e0cb4b
mayok1921 <mayok1921@gmail.com>
2026-07-02 18:17:35 +0900

    Add files via upload

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # DragoLearn
 
-Dark purple adaptive language learning PWA.
+Dragon-themed adaptive Japanese learning PWA with Hiragana, Katakana, Kanji, lesson groups, cumulative review, mastery tracking, multiple choice, typing, and flashcards.
--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,1 @@
-const CACHE_NAME='dragolearn-dark-v4';
-const FILES_TO_CACHE=['./','./index.html','./manifest.json','./service-worker.js','./icon-192.png','./icon-512.png'];
-self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(FILES_TO_CACHE)))});
-self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))))});
-self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))});
+const CACHE_NAME='dragolearn-full-v6';const FILES=['./','./index.html','./app.js','./manifest.json','./service-worker.js','./icon-192.png','./icon-512.png'];self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(FILES)))});self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))))});self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))});
